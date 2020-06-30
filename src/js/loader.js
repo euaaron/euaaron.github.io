badf5111da07caf5bd1effa0c1899715adbe988e
Aaron <euaaron@outlook.com>
2020-06-30 20:31:18 -0300

    Exercise update
    Corrected wrong outdated urls and changed 2018 exercises to 2019.

--- src/js/loader.js
+++ src/js/loader.js
@@ -12,7 +12,16 @@ function atividade_1(){
   page.style.display = "none";
   otherpage.display = "block";
   otherPage.src = "http://euaaron.github.io/site";
-  otherPage.src = "http://euaaron.github.io/faculdade/d_web/aula-1/";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/1/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_2(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/2/";
   loading.style.display = "block";
   document.getElementById('btnVoltar').style.display = "block";
 }
@@ -21,7 +30,7 @@ function atividade_3(){
   page.style.display = "none";
   otherpage.display = "block";
   otherPage.src = "http://euaaron.github.io/site";
-  otherPage.src = "http://euaaron.github.io/faculdade/d_web/aula-3/";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/3/";
   loading.style.display = "block";
   document.getElementById('btnVoltar').style.display = "block";
 }
@@ -30,7 +39,25 @@ function atividade_4(){
   page.style.display = "none";
   otherpage.display = "block";
   otherPage.src = "http://euaaron.github.io/site";
-  otherPage.src = "http://euaaron.github.io/faculdade/d_web/aula-4/";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/4/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_5(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/5/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_6(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/6/";
   loading.style.display = "block";
   document.getElementById('btnVoltar').style.display = "block";
 }
@@ -39,16 +66,133 @@ function atividade_7(){
   page.style.display = "none";
   otherpage.display = "block";
   otherPage.src = "http://euaaron.github.io/site";
-  otherPage.src = "http://euaaron.github.io/faculdade/d_web/aula-7/";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/7/";
   loading.style.display = "block";
   document.getElementById('btnVoltar').style.display = "block";
 }
 
-function javascript_1(){
+function atividade_8(){
   page.style.display = "none";
   otherpage.display = "block";
   otherPage.src = "http://euaaron.github.io/site";
-  otherPage.src = "http://euaaron.github.io/faculdade/d_web/js-1/";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/8/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_9(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/9/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_10(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/10/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_11(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/11/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_12(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/12/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_13(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/13/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_14(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/14/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_15(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/15/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_16(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/16/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_17(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/17/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function atividade_18(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/exercicio/18/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function prova_1(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/prova/aaron/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function prova_2(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/prova-2/pages/";
+  loading.style.display = "block";
+  document.getElementById('btnVoltar').style.display = "block";
+}
+
+function trabalho_final(){
+  page.style.display = "none";
+  otherpage.display = "block";
+  otherPage.src = "http://euaaron.github.io/site";
+  otherPage.src = "http://euaaron.github.io/faculdade/d-web/2019/trabalho-final/";
   loading.style.display = "block";
   document.getElementById('btnVoltar').style.display = "block";
 }
