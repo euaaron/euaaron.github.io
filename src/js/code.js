e326320acf2360baf51a37cfba2ffa2eacbc6076
Aaron <euaaron@outlook.com>
2020-06-30 23:08:36 -0300

    Removed resizing bugs.

--- src/js/code.js
+++ src/js/code.js
@@ -1,15 +1,11 @@
 var data = new Date();
-document.getElementById('ano').innerHTML = data.getFullYear();
+document.getElementById('ano').innerHTML = `2018 - ${data.getFullYear()}`;
 
 var blog = document.getElementById('blog');
 var projetos = document.getElementById('projetos');
 var faculdade = document.getElementById('faculdade');
+var tutoriais = document.getElementById('tutoriais');
 var btn = document.getElementById('menuBtn');
-var ativ1 = document.getElementById('ativ1');
-var ativ3 = document.getElementById('ativ3');
-var ativ4 = document.getElementById('ativ4');
-var ativ7 = document.getElementById('ativ7');
-var js1 = document.getElementById('js1');
 var subtitle = document.getElementById('pageSubtitle');
 
 if (btn.classList.contains("active")) {
@@ -18,15 +14,11 @@ if (btn.classList.contains("active")) {
 
 btn.style.cursor = "pointer";
 btnVoltar.style.cursor = "pointer";
-ativ1.style.cursor = "pointer";
-ativ3.style.cursor = "pointer";
-ativ4.style.cursor = "pointer";
-ativ7.style.cursor = "pointer";
-js1.style.cursor = "pointer";
 
 function goto_blog() {
     blog.style.display = "block";
     projetos.style.display = "none";
+    tutoriais.style.display = "none";
     faculdade.style.display = "none";
     subtitle.innerHTML = "Bem-vindo(a) de volta ao blog daquele cara chato.";
 }
@@ -34,13 +26,23 @@ function goto_blog() {
 function goto_projetos() {
     projetos.style.display = "block";
     blog.style.display = "none";
+    tutoriais.style.display = "none";
     faculdade.style.display = "none";
     subtitle.innerHTML = "kkkk... até parece que isso vai pra frente.";
+}
+
+function goto_tutoriais() {
+  tutoriais.style.display = "block";
+  tutoriais.style.display = "none";
+  blog.style.display = "none";
+  faculdade.style.display = "none";
+  subtitle.innerHTML = "kkkk... até parece que isso vai pra frente.";
 }
 
 function goto_faculdade() {
     faculdade.style.display = "block";
     blog.style.display = "none";
+    tutoriais.style.display = "none";
     projetos.style.display = "none";
     subtitle.innerHTML = "\"Ah, então é isso que o meu filho faz na faculdade!\", mãe do autor.";
 
