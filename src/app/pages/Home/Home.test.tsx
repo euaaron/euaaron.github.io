1e66888709f81c1081bcc56106188cc96505b7c8
Aaron Carneiro <git@aaroncarneiro.com>
2022-02-01 22:42:57 -0300

    fix: vercel routing 4a

--- src/app/pages/Home/Home.test.tsx
+++ src/app/pages/Home/Home.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Home } from "./Home";
 
 describe("Testing Home", () => {
   it("should render learn react link", () => {
     render(
-      <HashRouter>
+      <BrowserRouter>
         <Home />
-      </HashRouter>
+      </BrowserRouter>
     );
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
--- src/app/shared/http/routes/RootRoutes.spec.tsx
+++ src/app/shared/http/routes/RootRoutes.spec.tsx
@@ -1,16 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
-import { HashRouter, Link } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import { SystemRouter } from "./SystemRouter";
 
 describe("Testing SytemRouter", () => {
   it("should redirect to 404 page when trying to access an inexistent path", () => {
     render(
-      <HashRouter>
+      <BrowserRouter>
         <SystemRouter />
         <Link to="/UmaRotaInexistente/7a6d7asd">Error Page</Link>
-      </HashRouter>
+      </BrowserRouter>
     );
 
     const click = { button: 0 };
